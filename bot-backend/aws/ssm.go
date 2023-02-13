d039ef6ddc67c2982fb629fa772be49dd7ea34a6
EdgeJay <hjwu85@gmail.com>
2023-02-13 23:57:13 +0800

    add funcs to get params from ssm

---
+++ bot-backend/aws/ssm.go
@@ -0,0 +1,22 @@
+package aws
+
+import (
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ssm"
+	"github.com/aws/aws-sdk-go/service/ssm/ssmiface"
+)
+
+func GetSSMServiceClient() ssmiface.SSMAPI {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{}))
+	svc := ssm.New(sess)
+	return svc
+}
+
+func GetParameter(svc ssmiface.SSMAPI, name *string) (*ssm.GetParameterOutput, error) {
+	results, err := svc.GetParameter(
+		&ssm.GetParameterInput{
+			Name: name,
+		},
+	)
+	return results, err
+}
