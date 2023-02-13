355a3aa766cce702e58966b715cf66fbbdd5a2f9
EdgeJay <hjwu85@gmail.com>
2023-02-13 23:57:43 +0800

    add methods to get and set jwt token in cookie

--- bot-backend/cookies/cookies.go
+++ bot-backend/cookies/cookies.go
@@ -7,6 +7,8 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+const jwtCookieName = "token"
+
 func SetLaxSameSiteCookie(c *gin.Context, params gin.H, cookieName, path, domain string, duration int, httpOnly bool) error {
 	if cookieValue, err := json.Marshal(params); err != nil {
 		return err
@@ -25,4 +27,17 @@ func SetStrictSameSiteCookie(c *gin.Context, params gin.H, cookieName, path, dom
 		c.SetCookie(cookieName, string(cookieValue), duration, path, domain, true, httpOnly)
 	}
 	return nil
+}
+
+func SetJwtCookie(c *gin.Context, token, domain string, duration int) {
+	c.SetSameSite(http.SameSiteStrictMode)
+	c.SetCookie(jwtCookieName, token, duration, "/", domain, true, true)
+}
+
+func GetJwtCookie(c *gin.Context) (string, error) {
+	token, err := c.Cookie(jwtCookieName)
+	if err != nil {
+		return "", err
+	}
+	return token, nil
 }
