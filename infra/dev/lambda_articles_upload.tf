3bdb7a18361bac947a7740ce1f08447b0d9d946e
EdgeJay <hjwu85@gmail.com>
2023-03-06 00:28:07 +0800

    increase lambda function timeout

--- infra/dev/lambda_articles_upload.tf
+++ infra/dev/lambda_articles_upload.tf
@@ -14,6 +14,7 @@ resource "aws_lambda_function" "articles_upload" {
     source_code_hash = data.archive_file.lambda_articles_upload_zip.output_base64sha256
     runtime          = "go1.x"
     role             = aws_iam_role.lambda_exec_articles_upload.arn
+    timeout          = 120
 
     environment {
       variables = {
